9e194a9de527d735f621f1c66bd06e553950c181
Avishi Gupta <avishig012@gmail.com>
2020-08-08 16:53:09 +0530

    Create Player

---
+++ scenes/Actors/actor.gd
@@ -0,0 +1,13 @@
+extends KinematicBody2D
+
+class_name Actor
+
+export var speed: = Vector2(300.0, 1000.0)
+export var GRAVITY: = 3000.0
+
+var velocity = Vector2.ZERO
+
+func _physics_process(delta):
+	velocity.y += GRAVITY * delta
+	velocity = move_and_slide(velocity) 
+	$player.play("run")
