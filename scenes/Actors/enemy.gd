c64c6e5f7a0a55ee1aaf7e140f02df0d9d4b3516
Avishi Gupta <avishig012@gmail.com>
2020-08-09 14:06:23 +0530

    Delete enemy on attack

--- scenes/Actors/enemy.gd
+++ scenes/Actors/enemy.gd
@@ -1,12 +1,24 @@
 extends "res://Actors/actor.gd"
 
 func _ready() -> void:
-	velocity.x = -speed.x 
+	velocity.x = -speed.x
+	$enemy.play("walk")
+	get_node("enemy").set_flip_h(true)
+	set_process(true)
+	
+func _process(delta):
+	if (velocity.x >= 0.0):
+		get_node("enemy").set_flip_h(false)
+		
+	if (velocity.x < 0.0):
+		get_node("enemy").set_flip_h(true)
 
 func _on_AttackDetector_body_shape_entered(body_id, body, body_shape, area_shape):
-	if body.global_position.y > get_node("AttackDetector").global_position.y:
+	if body.global_position.y > get_node("AttackDetector").global_position.y or body.global_position.x < get_node("AttackDetector").global_position.y:
 		return
-	queue_free()
+	
+	if Input.is_key_pressed(KEY_SHIFT):
+		queue_free()
 
 func _physics_process(delta: float) -> void:
 	velocity.y += GRAVITY * delta
