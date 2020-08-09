70da91174fd1e446d13c45c79713993836854932
Avishi Gupta <avishig012@gmail.com>
2020-08-09 13:49:17 +0530

    Add attack functionality

--- scenes/Actors/enemy.gd
+++ scenes/Actors/enemy.gd
@@ -3,6 +3,11 @@ extends "res://Actors/actor.gd"
 func _ready() -> void:
 	velocity.x = -speed.x 
 
+func _on_AttackDetector_body_shape_entered(body_id, body, body_shape, area_shape):
+	if body.global_position.y > get_node("AttackDetector").global_position.y:
+		return
+	queue_free()
+
 func _physics_process(delta: float) -> void:
 	velocity.y += GRAVITY * delta
 	
@@ -10,3 +15,5 @@ func _physics_process(delta: float) -> void:
 		velocity.x *= -1.0
 		
 	velocity.y = move_and_slide(velocity, Vector2.UP).y	
+
+
