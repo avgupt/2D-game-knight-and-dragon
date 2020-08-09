933116f30e3063ccb0a7b3d52f8ef190b7bb0b42
Avishi Gupta <avishig012@gmail.com>
2020-08-09 12:53:29 +0530

    Add movement and visibility

--- scenes/Actors/enemy.gd
+++ scenes/Actors/enemy.gd
@@ -4,7 +4,9 @@ func _ready() -> void:
 	velocity.x = -speed.x 
 
 func _physics_process(delta: float) -> void:
+	velocity.y += GRAVITY * delta
+	
 	if is_on_wall():
 		velocity.x *= -1.0
 		
-	velocity = move_and_slide(velocity, Vector2.UP)		
+	velocity.y = move_and_slide(velocity, Vector2.UP).y	
